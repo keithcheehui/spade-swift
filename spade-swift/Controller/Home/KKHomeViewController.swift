095593799f2e171a5b7c31ad3ecc1f73973d704f
Wong Sai Khong <sk.wong@trinitywizards.com>
2021-04-22 18:31:02 +0800

    Home view controller wireframe

---
+++ spade-swift/Controller/Home/KKHomeViewController.swift
@@ -0,0 +1,75 @@
+//
+//  KKHomeViewController.swift
+//  spade-swift
+//
+//  Created by Wong Sai Khong on 22/04/2021.
+//
+
+import Foundation
+import UIKit
+
+class KKHomeViewController: KKBaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        initialLayout()
+    }
+    
+    func initialLayout(){
+        
+    }
+
+    ///Button Actions
+    @IBAction func btnProfileDidPressed(){
+        
+    }
+    
+    @IBAction func btnCopyDidPressed(){
+        
+    }
+
+    @IBAction func btnRefreshDidPressed(){
+        
+    }
+    
+    @IBAction func btnMissionDidPressed(){
+        
+    }
+    
+    @IBAction func btnBonusDidPressed(){
+        
+    }
+    
+    @IBAction func btnSettingsDidPressed(){
+        
+    }
+    
+    @IBAction func btnAffiliatesDidPressed(){
+        
+    }
+    
+    @IBAction func btnRebateDidPressed(){
+        
+    }
+    
+    @IBAction func btnMessageDidPressed(){
+        
+    }
+    
+    @IBAction func btnSupportDidPressed(){
+        
+    }
+    
+    @IBAction func btnMoreDidPressed(){
+        
+    }
+    
+    @IBAction func btnDepositDidPressed(){
+        
+    }
+    
+    @IBAction func btnWithdrawDidPressed(){
+        
+    }
+}
