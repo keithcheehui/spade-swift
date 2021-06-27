8f7d113b005cdf8a0d1cf8ae152b08555a59a9d1
Wong Sai Khong <sk.wong@trinitywizards.com>
2021-06-28 02:27:01 +0800

    updated settings toggles

--- spade-swift/Controller/Settings/KKSettingsViewController.swift
+++ spade-swift/Controller/Settings/KKSettingsViewController.swift
@@ -30,6 +30,9 @@ class KKSettingsViewController: KKBaseViewController {
     @IBOutlet weak var containerMarginRight: NSLayoutConstraint!
     @IBOutlet weak var titleMarginTop: NSLayoutConstraint!
     @IBOutlet weak var menuItemMarginLeft: NSLayoutConstraint!
+    
+    @IBOutlet weak var titleViewHeight: NSLayoutConstraint!
+    @IBOutlet weak var volumeContentViewMarginLeft: NSLayoutConstraint!
 
     ///Change Password Subview
     @IBOutlet weak var passwordContentView: UIView!
@@ -64,14 +67,35 @@ class KKSettingsViewController: KKBaseViewController {
     @IBOutlet weak var soundSectionView: UIView!
     @IBOutlet weak var lblSoundSettings: UILabel!
     @IBOutlet weak var lblMusic: UILabel!
-    @IBOutlet weak var lblSound: UILabel!
-    @IBOutlet weak var musicSlider: UISlider!
-    @IBOutlet weak var soundSlider: UISlider!
-    
-    @IBOutlet weak var titleViewHeight: NSLayoutConstraint!
-    @IBOutlet weak var volumeContentViewMarginLeft: NSLayoutConstraint!
-    @IBOutlet weak var volumeContentViewMarginRight: NSLayoutConstraint!
+    @IBOutlet weak var musicToggleView: UIView!
+    @IBOutlet weak var lblMusicToggleStatus: UILabel!
+    @IBOutlet weak var imgMusicToggle: UIImageView!
 
+    @IBOutlet weak var lblSound: UILabel!
+    @IBOutlet weak var soundToggleView: UIView!
+    @IBOutlet weak var lblSoundToggleStatus: UILabel!
+    @IBOutlet weak var imgSoundToggle: UIImageView!
+    
+    @IBOutlet weak var lblMute: UILabel!
+    @IBOutlet weak var muteToggleView: UIView!
+    @IBOutlet weak var lblMuteToggleStatus: UILabel!
+    @IBOutlet weak var imgMuteToggle: UIImageView!
+
+    @IBOutlet weak var toggleMarginLeft: NSLayoutConstraint!
+    @IBOutlet weak var toggleMarginRight: NSLayoutConstraint!
+    @IBOutlet weak var soundToggleMarginLeft: NSLayoutConstraint!
+    @IBOutlet weak var toggleContainerWidth: NSLayoutConstraint!
+    @IBOutlet weak var toggleViewHeight: NSLayoutConstraint!
+    @IBOutlet weak var imgToggleWidth: NSLayoutConstraint!
+    
+    @IBOutlet weak var imgMusicToggleMarginLeft: NSLayoutConstraint!
+    @IBOutlet weak var imgSoundToggleMarginLeft: NSLayoutConstraint!
+    @IBOutlet weak var imgMuteToggleMarginLeft: NSLayoutConstraint!
+
+    var musicToggleisOn = true
+    var soundToggleisOn = true
+    var muteToggleisOn = false
+    
     enum viewType: Int {
         case volumeSetting = 0
         case changePassword = 1
@@ -88,6 +112,10 @@ class KKSettingsViewController: KKBaseViewController {
         changePasswordLayout()
         
         buttonHover(type: viewType.volumeSetting.rawValue)
+        
+        muteToggleOnChanged()
+        musicToggleOnChanged()
+        soundToggleOnChanged()
     }
     
     func initialLayout(){
@@ -170,8 +198,8 @@ class KKSettingsViewController: KKBaseViewController {
         let version = KKSingleton.sharedInstance.appVersion
         if version != nil {
             lblVersionIOS.text = String(format: KKUtil.languageSelectedStringForKey(key: "settings_version_ios"), version!.appVersion!.isEmpty ? "" : version!.appVersion!)
-            lblVersionAndroid.text = String(format: KKUtil.languageSelectedStringForKey(key: "settings_version_android"), "")
-            lblVersionH5.text = String(format: KKUtil.languageSelectedStringForKey(key: "settings_version_h5"), "")
+            lblVersionAndroid.text = String(format: KKUtil.languageSelectedStringForKey(key: "settings_version_android"), version!.appVersion!.isEmpty ? "" : version!.appVersion!)
+            lblVersionH5.text = String(format: KKUtil.languageSelectedStringForKey(key: "settings_version_h5"), version!.appVersion!.isEmpty ? "" : version!.appVersion!)
         }
     }
     
@@ -179,18 +207,122 @@ class KKSettingsViewController: KKBaseViewController {
         soundSectionView.backgroundColor = UIColor(white: 0, alpha: 0.3)
         titleViewHeight.constant = KKUtil.ConvertSizeByDensity(size: 30)
         volumeContentViewMarginLeft.constant = KKUtil.ConvertSizeByDensity(size: 30)
-        volumeContentViewMarginRight.constant = KKUtil.ConvertSizeByDensity(size: 50)
 
         lblSoundSettings.text = KKUtil.languageSelectedStringForKey(key: "settings_volume_title")
         lblMusic.text = KKUtil.languageSelectedStringForKey(key: "settings_volume_music")
         lblSound.text = KKUtil.languageSelectedStringForKey(key: "settings_volume_sound")
         
         lblSoundSettings.font = UIFont.systemFont(ofSize: KKUtil.ConvertSizeByDensity(size: 12))
-        lblMusic.font = UIFont.systemFont(ofSize: KKUtil.ConvertSizeByDensity(size: 10))
+        lblMusic.font = UIFont.systemFont(ofSize: KKUtil.ConvertSizeByDensity(size: 12))
         lblSound.font = lblMusic.font
         
-        musicSlider.setThumbImage(UIImage(named: "ic_spade"), for: .normal)
-        soundSlider.setThumbImage(UIImage(named: "ic_spade"), for: .normal)
+        toggleMarginLeft.constant = KKUtil.ConvertSizeByDensity(size: 90)
+        toggleMarginRight.constant = KKUtil.ConvertSizeByDensity(size: 40)
+        soundToggleMarginLeft.constant = KKUtil.ConvertSizeByDensity(size: 30)
+        toggleContainerWidth.constant = KKUtil.ConvertSizeByDensity(size: 60)
+        toggleViewHeight.constant = KKUtil.ConvertSizeByDensity(size: 17)
+        imgToggleWidth.constant = KKUtil.ConvertSizeByDensity(size: 25)
+        
+        musicToggleView.layer.cornerRadius = toggleViewHeight.constant / 2
+        imgMusicToggle.image = UIImage(named: "ic_spade")
+        lblMusicToggleStatus.font = UIFont.systemFont(ofSize: KKUtil.ConvertSizeByDensity(size: 10))
+        
+        soundToggleView.layer.cornerRadius = toggleViewHeight.constant / 2
+        imgSoundToggle.image = UIImage(named: "ic_spade")
+        lblSoundToggleStatus.font = UIFont.systemFont(ofSize: KKUtil.ConvertSizeByDensity(size: 10))
+        
+        muteToggleView.layer.cornerRadius = toggleViewHeight.constant / 2
+        imgMuteToggle.image = UIImage(named: "ic_spade")
+        lblMuteToggleStatus.font = UIFont.systemFont(ofSize: KKUtil.ConvertSizeByDensity(size: 10))
+    }
+    
+    func musicToggleOnChanged(){
+        if (musicToggleisOn) {
+            musicToggleView.backgroundColor = .green
+            lblMusicToggleStatus.text = "On"
+            lblMusicToggleStatus.textAlignment = .left
+            lblMusicToggleStatus.textColor = .spade_black_000000
+            imgMusicToggleMarginLeft.constant = toggleContainerWidth.constant - imgToggleWidth.constant
+        } else {
+            musicToggleView.backgroundColor = .darkGray
+            lblMusicToggleStatus.text = "Off"
+            lblMusicToggleStatus.textAlignment = .right
+            lblMusicToggleStatus.textColor = .spade_white_FFFFFF
+            imgMusicToggleMarginLeft.constant = 0
+        }
+    }
+    
+    func soundToggleOnChanged(){
+        if (soundToggleisOn) {
+            soundToggleView.backgroundColor = .green
+            lblSoundToggleStatus.text = "On"
+            lblSoundToggleStatus.textAlignment = .left
+            lblSoundToggleStatus.textColor = .spade_black_000000
+            imgSoundToggleMarginLeft.constant = toggleContainerWidth.constant - imgToggleWidth.constant
+        } else {
+            soundToggleView.backgroundColor = .darkGray
+            lblSoundToggleStatus.text = "Off"
+            lblSoundToggleStatus.textAlignment = .right
+            lblSoundToggleStatus.textColor = .spade_white_FFFFFF
+            imgSoundToggleMarginLeft.constant = 0
+        }
+    }
+    
+    func muteToggleOnChanged(){
+        if (muteToggleisOn) {
+            muteToggleView.backgroundColor = .green
+            lblMuteToggleStatus.text = "On"
+            lblMuteToggleStatus.textAlignment = .left
+            lblMuteToggleStatus.textColor = .spade_black_000000
+            imgMuteToggleMarginLeft.constant = toggleContainerWidth.constant - imgToggleWidth.constant
+        } else {
+            muteToggleView.backgroundColor = .darkGray
+            lblMuteToggleStatus.text = "Off"
+            lblMuteToggleStatus.textAlignment = .right
+            lblMuteToggleStatus.textColor = .spade_white_FFFFFF
+            imgMuteToggleMarginLeft.constant = 0
+        }
+    }
+    
+    @IBAction func btnMusicToggleDidPressed(){
+        musicToggleisOn = !musicToggleisOn
+        
+        if (!musicToggleisOn && !soundToggleisOn) {
+            muteToggleisOn = true
+        } else {
+            muteToggleisOn = false
+        }
+
+        musicToggleOnChanged()
+        muteToggleOnChanged()
+    }
+    
+    @IBAction func btnSoundToggleDidPressed(){
+        soundToggleisOn = !soundToggleisOn
+        if (!musicToggleisOn && !soundToggleisOn) {
+            muteToggleisOn = true
+        } else {
+            muteToggleisOn = false
+        }
+        
+        soundToggleOnChanged()
+        muteToggleOnChanged()
+    }
+    
+    @IBAction func btnMuteToggleDidPressed(){
+        muteToggleisOn = !muteToggleisOn
+        
+        if (muteToggleisOn) {
+            musicToggleisOn = false
+            soundToggleisOn = false
+        } else {
+            musicToggleisOn = true
+            soundToggleisOn = true
+        }
+        
+        muteToggleOnChanged()
+        musicToggleOnChanged()
+        soundToggleOnChanged()
     }
     
     ///Button Actions
